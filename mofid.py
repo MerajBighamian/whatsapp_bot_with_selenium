5a94c3c14e36afaaef26e56b0b1f257e5e5d047d
Meraj <57762033+MerajBighamian@users.noreply.github.com>
2020-11-01 18:30:36 +0330

    this delete file for anti attack

--- mofid.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-import time
-
-driver = webdriver.Chrome(r'D:\selenium\selenium-app\chromedriver.exe')
-
-driver.get('https://onlineplus.mofidonline.com/Account/Login')
-driver.find_element_by_name('username').send_keys('mfdonline2366818')
-driver.find_element_by_name('password').send_keys('merajbighamian@09373951511//2581382')
-driver.find_element_by_xpath('').send_keys()
